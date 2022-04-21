f2535c6c2d64e16be59cbcce8e4e343cb62cf828
imedrahal <imedrahal1996@gmail.com>
2022-04-21 02:43:06 +0200

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <NavBar/>
+  <router-view/>
+  </div>
+</template>
+
+
+<script>
+import NavBar from './views/NavBar'
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+<style>
+@import '~bootstrap/dist/css/bootstrap.min.css'
+</style>
