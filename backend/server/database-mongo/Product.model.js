393c0f5e6f18fe28263c719d8a2b8a7b128601f1
imedrahal <imedrahal1996@gmail.com>
2022-04-21 16:33:57 +0200

    modification of signup and login

--- backend/server/database-mongo/Product.model.js
+++ backend/server/database-mongo/Product.model.js
@@ -4,8 +4,6 @@ const db = require("./index.js");
 const ProductSchema = new mongoose.Schema({
     title: String,
     description: String,
-    // email: {unique: true,
-    //   type: String}
     image_url: String,
     quantite: Number,
     user_id: String
