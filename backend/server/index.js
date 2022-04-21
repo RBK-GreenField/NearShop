39f6e9250f543620685d5c94ed9ab98b7d4b4a63
amine <amine.selmi@esprit.tn>
2022-04-21 13:45:59 +0100

    initializing schema and backend organisation

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userRoutes = require('./routes/user.routes')
+const commentRoutes = require('./routes/comment.routes')
+const productRoutes = require('./routes/product.routes')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
@@ -13,9 +15,11 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + "/../client/public"));
 
-app.use("/api/Users", userRoutes);
+
+app.use("/api/users", userRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, function () {
   console.log("listening on port 3000!");
