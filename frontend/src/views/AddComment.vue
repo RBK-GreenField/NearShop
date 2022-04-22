b988d3c1017d2a3e11155468b11aec384771d46f
amine <amine.selmi@esprit.tn>
2022-04-22 01:33:50 +0100

    add comment

---
+++ frontend/src/views/AddComment.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="field">
+      <label class="label">comment here</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="write ..."
+          v-model="productName"
+        />
+      </div>
+    </div>
+
+    <div class="control">
+      <button class="button is-success" @click="saveProduct">SEND</button>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+    name: 'add-product',
+    data(){
+        return{
+            description:'',
+        };
+    },
+    methods: {
+        async saveComment(){
+            try{
+                await axios.post('',{
+                description: this.description
+            })
+            this.comment='';
+            this.$router.push('/');
+            }catch(err){
+                console.log(err)
+            }
+        },
+    },
+}
+</script>
+<style>
+
+</style>
