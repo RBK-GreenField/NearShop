e64ea59c7c863e86283ce09e18aca56037f44e58
amine <amine.selmi@esprit.tn>
2022-04-22 03:14:53 +0100

    fixing problem

--- frontend/src/views/AddComment.vue
+++ frontend/src/views/AddComment.vue
@@ -1,46 +1,50 @@
 <template>
   <div>
-    <div class="field">
-      <label class="label">comment here</label>
-      <div class="control">
-        <input
-          class="input"
-          type="text"
-          placeholder="write ..."
-          v-model="productName"
-        />
+    <div>
+      <div>
+        <div class="field">
+          <label class="label">comment here</label>
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              placeholder="write ..."
+              v-model="productName"
+            />
+          </div>
+        </div>
+        <div class="control">
+          <button class="button is-success" @click="saveProduct">SEND</button>
+        </div>
       </div>
-    </div>
-
-    <div class="control">
-      <button class="button is-success" @click="saveProduct">SEND</button>
+      <div>
+        <p>list</p>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-    name: 'add-product',
-    data(){
-        return{
-            description:'',
-        };
+  name: "AddComment",
+  data() {
+    return {
+      description: "",
+    };
+  },
+  methods: {
+    async saveComment() {
+      try {
+        await axios.post("", {
+          description: this.description,
+        });
+        this.comment = "";
+        this.$router.push("/");
+      } catch (err) {
+        console.log(err);
+      }
     },
-    methods: {
-        async saveComment(){
-            try{
-                await axios.post('',{
-                description: this.description
-            })
-            this.comment='';
-            this.$router.push('/');
-            }catch(err){
-                console.log(err)
-            }
-        },
-    },
-}
+  },
+};
 </script>
-<style>
-
-</style>
+<style></style>
