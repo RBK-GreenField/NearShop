c4ff9a51c61847bab240076ff3542b714b4a2c43
Mourad-Chniti <mouradchniti9@gmail.com>
2022-04-26 10:35:40 +0100

    shoppingCart

---
+++ frontend/src/components/shoppingCart.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="mix">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header menuitem">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a
+                href="#"
+                class="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-expanded="false"
+              >
+                <span class="fa fa-gift bigicon"></span> 4 - Items in Cart<span
+                  class="caret"
+                ></span
+              ></a>
+              <ul class="dropdown-menu dropdown-cart" role="menu">
+                <li>
+                  <span class="item">
+                    <span class="item-left">
+                      <span class="item-info">
+                        <span></span>
+                        <span></span>
+                      </span>
+                    </span>
+                    
+                    <!-- <span class="item-right">
+                      <button class="btn btn-danger fa fa-close"></button>
+                    </span>
+                  </span>
+                </li>
+                <li>
+                  <span class="item">
+                    <span class="item-left">
+                      <img
+                        src="http://www.prepbootstrap.com/Content/images/template/menucartdropdown/item_2.jpg"
+                        alt=""
+                      />
+                      <span class="item-info">
+                        <span>Item name</span>
+                        <span>price: 3$</span>
+                      </span>
+                    </span>
+                    <span class="item-right">
+                      <button class="btn btn-danger fa fa-close"></button>
+                    </span>
+                  </span>
+                </li>
+                <li>
+                  <span class="item">
+                    <span class="item-left">
+                      <img
+                        src="http://www.prepbootstrap.com/Content/images/template/menucartdropdown/item_3.jpeg"
+                        alt=""
+                      />
+                      <span class="item-info">
+                        <span>Item name</span>
+                        <span>price: 12$</span>
+                      </span>
+                    </span>
+                    <span class="item-right">
+                      <button class="btn btn-danger fa fa-close"></button>
+                    </span>
+                  </span>
+                </li>
+                <li>
+                  <span class="item">
+                    <span class="item-left">
+                      <img
+                        src="http://www.prepbootstrap.com/Content/images/template/menucartdropdown/item_4.jpg"
+                        alt=""
+                      />
+                      <span class="item-info">
+                        <span>Item name</span>
+                        <span>price: 7$</span>
+                      </span>
+                    </span>
+                    <span class="item-right">
+                      <button class="btn btn-danger fa fa-close"></button>
+                    </span>
+                  </span> -->
+                </li>
+                <li class="dropdown-divider"></li>
+                <li><a class="text-xs-center" href="#">View Cart</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+// import $ from "jquery";
+export default {
+  name: "ShoppingCart",
+  methods: {
+    // drop() {
+    //   $("#e").on("click", function () {
+    //     $(".shopping-cart").toggle("slow");
+    //   });
+    // },
+  },
+};
+</script>
+
+<style>
+.bigicon {
+  color: white;
+}
+
+.mix {
+  min-height: 370px;
+}
+
+ul.dropdown-cart {
+  min-width: 250px;
+  border: 2px solid #343434;
+  padding: 2px;
+  margin: 7px;
+  margin-top: 11px;
+}
+ul.dropdown-cart li .item {
+  display: block;
+  padding: 3px 10px;
+  margin: 3px 0;
+}
+ul.dropdown-cart li .item:hover {
+  background-color: #c3c5c5;
+}
+ul.dropdown-cart li .item:after {
+  visibility: hidden;
+  display: block;
+  font-size: 0;
+  content: " ";
+  clear: both;
+  height: 0;
+}
+
+ul.dropdown-cart li .item-left {
+  float: left;
+}
+ul.dropdown-cart li .item-left img,
+ul.dropdown-cart li .item-left span.item-info {
+  float: left;
+}
+ul.dropdown-cart li .item-left span.item-info {
+  margin-left: 10px;
+}
+ul.dropdown-cart li .item-left span.item-info span {
+  display: block;
+}
+ul.dropdown-cart li .item-right {
+  float: right;
+}
+ul.dropdown-cart li .item-right button {
+  margin-top: 14px;
+}
+</style>
--- frontend/src/views/NavBar.vue
+++ frontend/src/views/NavBar.vue
@@ -1,69 +1,84 @@
 <template>
-
- <nav class="navbar navbar-expand-lg navbar-light" >
-  <div class="container-fluid">
-  
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"  >
-          <router-Link class="nav-link active" aria-current="page" to="/">Home</router-link>
-        </li>
-        <li class="nav-item" >
-          <router-Link class="nav-link" to="/about">About</router-Link>
-        </li>
-        <li class="nav-item" v-if="user==null" style="margin-left:510%">
-          <router-Link class="nav-link" to="/SigNup">SigNup</router-Link>
-        </li>
-        <li class="nav-item" v-if="user==null">
-          <router-Link class="nav-link" to="/LogIn">LogIn</router-Link>
-        </li>
-         <li class="nav-item" v-if="user">
-          <router-Link class="nav-link" to="/All-Products">All-Products</router-Link>
-        </li>
-         <li class="nav-item" v-if="user">
-          <router-Link class="nav-link" to="/MyProducts">MyProduct</router-Link>
-        </li>
-        <li class="nav-item" v-if="user">
-          <router-Link class="nav-link" to="/Add-Product">Add-Product</router-Link>
-        </li>
-        <li class="nav-item" v-if="user"  style="margin-left:1000px">
-          <router-Link class="nav-link" to="/LogOut">LogOut</router-Link>
-        </li>
-        
-       </ul>
-   
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-Link class="nav-link active" aria-current="page" to="/"
+              >Home</router-Link
+            >
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" to="/about">About</router-Link>
+          </li>
+          <li class="nav-item" v-if="user == null" style="margin-left: 510%">
+            <router-Link class="nav-link" to="/SigNup">SigNup</router-Link>
+          </li>
+          <li class="nav-item" v-if="user == null">
+            <router-Link class="nav-link" to="/LogIn">LogIn</router-Link>
+          </li>
+          <li class="nav-item" v-if="user">
+            <router-Link class="nav-link" to="/All-Products"
+              >All-Products</router-Link
+            >
+          </li>
+          <li class="nav-item" v-if="user">
+            <router-Link class="nav-link" to="/MyProducts"
+              >MyProduct</router-Link
+            >
+          </li>
+          <li class="nav-item" v-if="user">
+            <router-Link class="nav-link" to="/Add-Product"
+              >Add-Product</router-Link
+            >
+          </li>
+          <li class="nav-item" v-if="user" style="margin-left: 1000px">
+            <router-Link class="nav-link" to="/LogOut">LogOut</router-Link>
+          </li>
+          <li>
+            <ShoppingCart />
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </template>
 
 <script>
-export default{
-  name:'NavBar',
-  data(){
-    return{
-      user:null
-      }
-    
+import ShoppingCart from "../components/shoppingCart.vue";
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      user: null,
+    };
+  },
+  components: {
+    ShoppingCart,
   },
   created() {
-    if (localStorage.getItem('token') != null) {
-      this.user=localStorage.getItem('token')
+    if (localStorage.getItem("token") != null) {
+      this.user = localStorage.getItem("token");
+    } else {
+      this.user = null;
     }
-    else{
-      this.user=null
-    }
-  }
-}
+  },
+};
 </script>
 
-
 <style>
-nav{
-background: rgb(203, 223, 223);
+nav {
+  background: rgb(203, 223, 223);
 }
 </style>
